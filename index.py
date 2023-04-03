539f5eba8b6ffecfdf01fbdfbcdbe32f3ebcca69
Visweswar <sunil@Visweswars-MacPro.local>
2023-04-03 23:02:44 +0530

    index.py

---
+++ index.py
@@ -0,0 +1,27 @@
+index = print("hello")
+
+print(index)
+
+
+# assign a string to a variable
+greeting = "Hello"
+
+
+# assign an integer to a variable
+number = 42
+
+# assign a float to a variable
+pi = 3.14159
+
+# assign a boolean to a variable
+is_valid = True
+
+# print the values of the variables
+print(greeting)
+print(number)
+print(pi)
+print(is_valid)
+
+# combine variables in a string
+message = f"{greeting}, the number is {number}, and pi is {pi}"
+print(message)
